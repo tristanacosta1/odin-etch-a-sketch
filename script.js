36e751a4b5be8b6c4b8c68f77935168062565839
Tristan <tristanacosta03@gmail.com>
2026-01-18 14:30:47 +0800

    Add 16 by 16 grid of divs

---
+++ script.js
@@ -0,0 +1,9 @@
+const INITIAL_GRID_SIZE = 16;
+const container = document.querySelector("#flex-container")
+
+// Create 16 by 16 grid
+for (let i = 0; i < INITIAL_GRID_SIZE * INITIAL_GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+}
