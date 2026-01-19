d0f1b1b26c8b2db35651832285fd7bff9def1e08
Tristan <tristanacosta03@gmail.com>
2026-01-19 13:13:34 +0800

    Finish main functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function makeGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.style.flexBasis = `calc(100% / ${size})`;
         container.appendChild(cell);
         cell.addEventListener('mouseenter', () => cell.classList.add("hover"));
     }   
@@ -20,19 +21,21 @@ button.classList.add("button");
 
 const newSize = button.addEventListener('click', () =>  {
     let userSize = prompt("Enter grid size.\n(Maximum: 100)");
-
     if (userSize === null) {
-            return;
+        return;
     }
 
-    while (Number.isInteger(userSize) || userSize > 100 || userSize < 1) {
+    userSize = Number(userSize);
+
+    while (!Number.isInteger(userSize) || userSize > 100 || userSize < 1) {
         userSize = prompt("Please enter a valid value!");
+        userSize = Number(userSize);
+
         if (userSize === null) {
-            break;
+            return;
         }
     }
+    container.replaceChildren();
 
-    return userSize;
+    makeGrid(userSize);
 });
-
-makeGrid(newSize);
